3c4d305ca8c1ed7ec40bb69303c41ba5b5ca5cfc
Kelt <kelt_dmitriy@gmail.com>
2023-04-27 13:13:52 +0300

    add Task 36 homework

---
+++ Task_num36/Program.cs
@@ -0,0 +1,52 @@
+﻿//Семинар 5.
+//Домашнее задание.
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов с нечётными индексами.
+//Выполнил: Кельт Дмитрий Владимирович (kelt_1981@mail.ru)
+//Решение:
+
+int ReadInt(string argument) 
+{
+    Console.Write(argument);
+    int i;
+    while (!int.TryParse(Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Вы ввели не число!");
+        Console.Write(argument);
+    }
+    return i;
+}
+int[] RandomArray(int length, int minValue = 1, int maxValue = 10) 
+{
+    Random rnd =new Random();
+    int[] array = new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        System.Console.Write($"{numbers[i]} ");
+    }
+    System.Console.WriteLine();
+}
+int Sum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int length = ReadInt("Введите длину массива: ");
+int[] arr = RandomArray(length);
+PrintArray(arr);
+System.Console.WriteLine($"Сумма элементов с нечетными индексами массива : {Sum(arr)}");
